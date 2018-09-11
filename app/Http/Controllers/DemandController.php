191eaffbcb7cbadf28d5cd849f961de4b4175d44
钟兆立 <525966315@qq.com>
2018-09-11 20:50:42 +0800

    动态

--- app/Http/Controllers/DemandController.php
+++ app/Http/Controllers/DemandController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+use App\User;
 
 class DemandController extends Controller
 {
     public function index(Request $request){
-        $a = DB::table("demand")->paginate(10);
+        $a = DB::table("demand")->paginate(7);
         return $a;
     }
 }
